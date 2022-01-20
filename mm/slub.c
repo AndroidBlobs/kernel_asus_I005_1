1ce6fde998b1c8dc9800746e5d6c98ff92f020a0
ShivamKumarJha <jha.shivam3@gmail.com>
2022-01-20 16:12:30 +0530

    Add mm/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- mm/slub.c
+++ mm/slub.c
@@ -4424,7 +4424,6 @@ void *__kmalloc_track_caller(size_t size, gfp_t gfpflags, unsigned long caller)
 
 	return ret;
 }
-EXPORT_SYMBOL(__kmalloc_track_caller);
 
 #ifdef CONFIG_NUMA
 void *__kmalloc_node_track_caller(size_t size, gfp_t gfpflags,
@@ -4455,7 +4454,6 @@ void *__kmalloc_node_track_caller(size_t size, gfp_t gfpflags,
 
 	return ret;
 }
-EXPORT_SYMBOL(__kmalloc_node_track_caller);
 #endif
 
 #ifdef CONFIG_SYSFS
@@ -6145,7 +6143,7 @@ static int alloc_trace_locations(struct seq_file *seq, struct kmem_cache *s,
 
 		seq_printf(seq,
 		"alloc_list: call_site=%pS count=%zu object_size=%zu slab_size=%zu slab_name=%s\n",
-			(void *)l->addr, l->count, s->object_size, s->size, s->name);
+			l->addr, l->count, s->object_size, s->size, s->name);
 #ifdef CONFIG_STACKTRACE
 		for (j = 0; j < TRACK_ADDRS_COUNT; j++)
 			if (l->addrs[j]) {
@@ -6323,9 +6321,16 @@ static int __init slab_sysfs_init(void)
 #ifdef CONFIG_SLUB_DEBUG
 	if (slub_debug) {
 		slab_debugfs_top = debugfs_create_dir("slab", NULL);
-		if (!IS_ERR(slab_debugfs_top))
-			debugfs_create_file("alloc_trace", 0400, slab_debugfs_top,
-					NULL, &slab_debug_alloc_fops);
+		if (!slab_debugfs_top) {
+			pr_err("Couldn't create slab debugfs directory\n");
+			return -ENODEV;
+		}
+
+		if (!debugfs_create_file("alloc_trace", 0400, slab_debugfs_top,
+					NULL, &slab_debug_alloc_fops)) {
+			pr_err("Couldn't create slab/tests debugfs directory\n");
+			return -ENODEV;
+		}
 	}
 #endif
 
